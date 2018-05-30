79f55a8909fbfb949124dab12f4efb7180861305
Jaqueline Espinoza <jaqueespinoza791@gmail.com>
2018-05-30 15:00:19 -0500

    agregar apy.py

---
+++ api.py
@@ -0,0 +1,5 @@
+class claves:
+    def __init__(self):
+        self.consultaEstados = "http://clima.inifap.gob.mx/wapi/api/Estacion?"
+        self.consultaEstaciones = "http://clima.inifap.gob.mx/wapi/api/Estacion?"
+        self.consultaDatos = "http://clima.inifap.gob.mx/wapi/api/Datos?"
