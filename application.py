b231aae2c2962125cf8956f8d4a5dd0accea2c8c
Carolina Briano <carolinabriano15@gmail.com>
2018-06-04 10:14:50 -0500

    Diseño de pagina Estacion

--- application.py
+++ application.py
@@ -1,33 +1,49 @@
-#LIbrerias
+#Se importan librerias
 import pandas as pd
 import json
 from urllib.request import urlopen
 from flask import Flask, render_template
-app=Flask(__name__) #asignacion de la página
-
-id=""
-@app.route('/') #asignacion del index
-def datos():
-    arr=[]
-    with urlopen('http://clima.inifap.gob.mx/wapi/api/Estacion?') as response:
-        source=response.read()
+from api import claves
+app = Flask(__name__)
+@app.route('/')#
+def index():
+    acceso=claves()
+    consulta=acceso.consultaEstados
+    #conUrl=("http://clima.inifap.gob.mx/wapi/api/Estacion?")
+    with urlopen(consulta) as response:source=response.read()
     data=json.loads(source)
-    df = pd.DataFrame(data['estados'])
-    return render_template('index.html',dato=df['Nombre'])
+    df=pd.DataFrame(data['estados'])
+    return render_template("index.html", dato=df['Nombre'])
 
 @app.route('/<string:name>/<string:id>')
-
 def est(name,id):
-    with urlopen('http://clima.inifap.gob.mx/wapi/api/Estacion?idEstado={}'.format(id)) as response:
+    acceso=claves()
+    consulta=acceso.consultaEstaciones
+    #consulta=("http://clima.inifap.gob.mx/wapi/api/Estacion?idEstado={}".format(id))
+    with urlopen("{}idEstado={}".format(consulta,id)) as response:
         source=response.read()
     data=json.loads(source)
-    df = pd.DataFrame(data['est'])
-    return render_template('estacion.html',dato=df['Nombre'])
+    df=pd.DataFrame(data['est'])
+    df
+    tac=df['Numero']
+    tac
+#estac=tac
+    #print(df)
+    return render_template('estacion.html',dato1=df['Nombre'],estado=name, num=id,estac=tac)
 
-@app.route('/<string:name>/<string:Numero>')
-def estaciones(name,numero):
-    with urlopen('http://clima.inifap.gob.mx/wapi/api/Datos?idEstado=1&IdEstacion={}'.format(numero)) as response:
+
+@app.route('/<string:estado>/<string:estacion>/<string:numesta>/<string:numero>')
+def estacion(estado,numesta,estacion,numero):
+    acceso=claves()
+    consulta=acceso.consultaDatos
+    print("****",estado,numesta,estacion,numero)
+        #llamada="http://clima.inifap.gob.mx/wapi/api/Datos?idEstado={}&IdEstacion={}".format(estacion,numero)
+        #print(llamada)
+    with urlopen("{}&IdEstado={}&IdEstacion={}".format(consulta,estacion,numero)) as response:
         source=response.read()
     data=json.loads(source)
-    df = pd.DataFrame(data['estaciones'])
-    return render_template('estaciones.html',dato=df['Nombre'])
+    dicc=[]
+    dicc=dict(data)
+
+        #print(df)
+    return render_template('estaciones.html',dato2=dicc)
