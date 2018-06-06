39b60a66deca4873df6e4cda762c97df92c7338a
Lucre Rodriguez <lucre1998@outlook.com>
2018-06-06 13:44:12 -0500

    06/06/2018 agregar indice de calor y punto de rocio

--- application.py
+++ application.py
@@ -34,10 +34,16 @@ def est(name,id):
 def estacion(fir,esta,estacion,numes):
     print(fir,"  ",esta,"  ",estacion,"  ",numes)
     acceso=claves()
-    cons=acceso.consultaDatos
+    cons= '{}idEstado={}&IdEstacion={}'.format(acceso.consultaDatos,esta,numes)
     print("***** consulta",cons)
-    with urlopen('{}idEstado={}&IdEstacion={}'.format(cons,esta,numes)) as response:
+    with urlopen(cons) as response:
         source=response.read()
     data=json.loads(source)
     d = dict(data)
-    return render_template('est.html',da2=d)
+    F=d['estaciones'][0]['Temt']
+    RH=d['estaciones'][0]['Humr']
+    T =F * 1.8000 + 32.00
+#IC = (-42.379 + (2.04901523 * T) + (10.14333127 * R) - (0.22475541 * (T*R))  - (6.83783e-3 * (T*T)) - (5.481717e-2 * (R*R)) + ( 1.22874e-3 * (T*T*R)) + (8.5282e-4 * (T*R*R)) - (1.99e-6 * (T*T*R*R)))
+    IC=  -42.379 + 2.04901523*T + 10.14333127*RH - .22475541*T*RH - .00683783*T*T - .05481717*RH*RH + .00122874*T*T*RH + .00085282*T*RH*RH - .00000199*T*T*RH*RH
+    Pr= ((RH/100)*(100+F)-100)/8
+    return render_template('pagina3_1.html',da2=d,c=IC,pr=Pr)
