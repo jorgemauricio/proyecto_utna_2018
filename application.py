94530f06754b810889bbd27f9fb15b188182fcd4
Carolina Briano <carolinabriano15@gmail.com>
2018-06-08 14:08:39 -0500

    Tercer Avance

--- application.py
+++ application.py
@@ -2,7 +2,7 @@
 import pandas as pd
 import json
 from urllib.request import urlopen
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from api import claves
 app = Flask(__name__)
 
@@ -17,6 +17,7 @@ def index():
     data=json.loads(source)
     df=pd.DataFrame(data['estados'])
     return render_template("index.html", dato=df['Nombre'])
+
 
 @app.route('/<string:name>/<string:id>')
 def est(name,id):
@@ -35,6 +36,7 @@ def est(name,id):
     return render_template('estacion.html',dato1=df['Nombre'],estado=name, num=id,estac=tac)
 
 
+
 @app.route('/<string:estado>/<string:estacion>/<string:numesta>/<string:numero>')
 def estacion(estado,numesta,estacion,numero):
     acceso=claves()
@@ -47,6 +49,21 @@ def estacion(estado,numesta,estacion,numero):
     data=json.loads(source)
     dicc=[]
     dicc=dict(data)
-
+    t= dicc['estaciones'][0]['Temt']#temperatura
+    h= dicc['estaciones'][0]['Humr']#humedad relativa
+    T=(9*int(t)/5)+32#conversion de °c a °f
+    HI = -42.379 + 2.04901523*T + 10.14333127*h - .22475541*T*h - .00683783*T*T - .05481717*h*h + .00122874*T*T*h + .00085282*T*h*h - .00000199*T*T*h*h
+    Pr= ((h/100)**(1/8))*(112+0.9*t)+0.1*t-112
         #print(df)
-    return render_template('estaciones.html',dato2=dicc)
+    return render_template('estaciones.html',dato2=dicc, dat3=HI,pr=Pr)
+
+@app.route("/")
+def url(u):
+    acceso=claves()
+    consulta=acceso.consultaEstados
+    #conUrl=("http://clima.inifap.gob.mx/wapi/api/Estacion?")
+    with urlopen(consulta) as response:source=response.read()
+    data=json.loads(source)
+    df=pd.DataFrame(data['estados'])
+    return redirect(url_for("index.html"), dato=df['Nombre'])
+    #return render_template("index.html", u=u)
