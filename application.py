61d6e37a21725f2486db85c66bf530f9f8d7007b
Alejandra García <ale-240498@hotmail.com>
2018-06-12 11:57:08 -0600

    Avance de las demas plantillas

---
+++ application.py
@@ -0,0 +1,44 @@
+#LIbrerias
+import pandas as pd
+import json
+
+from urllib.request import urlopen
+from flask import Flask, render_template
+from api import claves
+app=Flask(__name__) #asignacion de la página
+
+a=""
+@app.route('/') #asignacion del index
+def datos():
+    acceso=claves()
+    cons=acceso.consultaEstados
+    with urlopen(cons) as response:
+        source=response.read()
+    data=json.loads(source)
+    df = pd.DataFrame(data['estados'])
+    return render_template('index.html',da=df['Nombre'])
+#@app.route('/<string:name>')
+#def hello(name):
+    #return "<h1>Tu elegiste: {}</h1>".format(name.upper())
+@app.route('/<string:name>/<string:id>')
+def est(name,id):
+    acceso=claves()
+    cons=acceso.consultaEstaciones
+    with urlopen("{}idEstado={}".format(cons,id)) as response:
+        source=response.read()
+    data=json.loads(source)
+    df = pd.DataFrame(data['est'])
+    a=df['Numero']
+    return render_template('pagina.html',da1=df['Nombre'],estado=name,nums=id,estaci=a)
+
+@app.route('/<string:fir>/<string:esta>/<string:estacion>/<string:numes>')
+def estacion(fir,esta,estacion,numes):
+    print(fir,"  ",esta,"  ",estacion,"  ",numes)
+    acceso=claves()
+    cons= '{}idEstado={}&IdEstacion={}'.format(acceso.consultaDatos,esta,numes)
+    print("***** consulta",cons)
+    with urlopen(cons) as response:
+        source=response.read()
+    data=json.loads(source)
+    d = dict(data)
+    return render_template('pagina2.html',da2=d)
